c68c53a85bfaa90d31f6c1d171dbc9e5de1bb182
Marc <m.bluemner@liquidlabs.de>
2014-09-17 02:58:38 +0200

    can create projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ProjectManager::Application.routes.draw do
+
+  root "projects#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

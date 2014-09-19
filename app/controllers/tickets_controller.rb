0233e43262340be3fd477b698762c541ff7a8895
Marc <m.bluemner@liquidlabs.de>
2014-09-19 02:43:28 +0200

    can now edit tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -23,8 +23,11 @@ class TicketsController < ApplicationController
 
   def update
     if @ticket.update(ticket_params)
-      flash[:notice] = "TTicket has been updated"
+      flash[:notice] = "Ticket has been updated."
 
+      redirect_to [@project, @ticket]
+    else
+      flash[:alert] = "TTicket has not been updated."
       render action: "edit"
     end
   end
